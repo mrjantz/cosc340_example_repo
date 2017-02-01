c2eea021138d9f558a6ce552601efb84b6a839c6
drjantz <mrjantz@utk.edu>
2017-02-01 09:54:17 -0500

    going back to version 9fcead0

--- run_ref.sh
+++ run_ref.sh
@@ -5,6 +5,7 @@ rm -rf test_output/ref
 mkdir -p test_output/ref
 
 ./src/bzip2.exe input/ref/input.source 280 > test_output/ref/input.source.out 2>> test_output/ref/input.source.err
+./src/bzip2.exe input/ref/chicken.jpg 30 > test_output/ref/chicken.jpg.out 2>> test_output/ref/chicken.jpg.err
 ./src/bzip2.exe input/ref/liberty.jpg 30 > test_output/ref/liberty.jpg.out 2>> test_output/ref/liberty.jpg.err
 ./src/bzip2.exe input/ref/input.program 280 > test_output/ref/input.program.out 2>> test_output/ref/input.program.err
 ./src/bzip2.exe input/ref/text.html 280 > test_output/ref/text.html.out 2>> test_output/ref/text.html.err
